3ad5f33ed59dd2df16efc9b40be3727ce442669e
Jimmy ChangWenChi <geograp232@gmail.com>
2023-02-27 23:48:56 +0800

    finish

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -2,17 +2,41 @@
     這是後台頁面
     <router-link to="/admin/products">後臺產品列表</router-link> |
     <router-link to="/admin/orders">後臺訂單列表</router-link> |
-    <router-link to="/">回前台首頁</router-link> 
+    <router-link to="/">回前台首頁</router-link> |
+    <a href="#" @click.prevent="logout">登出</a>
     <hr>
     <RouterView></RouterView>
 </template>
 
 <script>
+
 import { RouterView } from 'vue-router'
+const { VITE_APP_URL } = import.meta.env;
 
 export default {
     components: {
         RouterView
+    },
+    methods: {
+        logout(){
+            document.cookie = `JimmyToken=;expired=${new Date()}; `;
+            this.$router.push("/login")
+        },
+        checkAdmin(){
+            const token = document.cookie.replace(/(?:(?:^|.*;\s*)JimmyToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+            this.$http.defaults.headers.common.Authorization = token;
+
+            const url = `${VITE_APP_URL}/api/user/check`;
+            this.$http.post(url)
+            .then((res) =>{
+              if(!res.data.success){
+                  this.$router.push("/login")
+              }  
+            })
+        },
+    },
+    mounted(){
+        this.checkAdmin()
     }
 }
 </script>
--- src/views/front/LoginView.vue
+++ src/views/front/LoginView.vue
@@ -1,10 +1,69 @@
 <template>
-    這是登入頁面
+  這是登入頁面
+  <form id="form" class="form-signin">
+    <div class="form-floating mb-3">
+      <input
+        type="email"
+        class="form-control"
+        v-model="user.username"
+        id="floatingInput"
+        placeholder="name@example.com"
+        required
+        autofocus
+      />
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input
+        type="password"
+        class="form-control"
+        v-model="user.password"
+        id="floatingPassword"
+        placeholder="Password"
+        required
+      />
+      <label for="floatingPassword">Password</label>
+    </div>
+    <button
+      class="btn btn-lg btn-primary w-100 mt-3"
+      type="button"
+      v-on:click="login"
+    >
+      登入
+    </button>
+  </form>
 </template>
 
 
 <script>
+const { VITE_APP_URL,VITE_APP_PATH } = import.meta.env;
+
 export default {
-    
-}
+    data(){
+        return {
+            user: {
+                username:"",
+                password:""
+            },
+            test:"test"
+        }
+    },
+    methods: {
+        login(){
+            //const apiPath = "https://vue3-course-api.hexschool.io/v2/admin/signin";
+            const apiPath = `${VITE_APP_URL}/v2/admin/signin`;
+            
+            this.$http.post(apiPath,this.user)
+            .then((res) => {
+                const { token,expired } = res.data;
+                document.cookie = `JimmyToken=${token};expired=${new Date(expired)}; `;
+                //window.location = `upload_image.html`;
+                this.$router.push("/admin/products")
+            })
+            .catch((err)=>{
+                alert(err.response.data.message);
+            })
+        },
+    }
+};
 </script>
